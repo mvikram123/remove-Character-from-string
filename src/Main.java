744aed9d579d1e503c764f49eabfd23e9ba62c14
viccky8578 <108965142+viccky8578@users.noreply.github.com>
2023-03-07 14:22:32 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -32,3 +32,32 @@ public class Main {
         obj.removeChars(S);
     }
 }
+//general method for remving character from string
+// class Main{
+// public static void main(String args[]){
+// Scanner sc=new Scanner(System.in);
+// String s=sc.next();
+// String res=" ";
+// for (int i = 0; i < s.length(); i++) {
+// if(s.charAt(i)=='a'||s.charAt(i)=='A'){
+// res=res+"";//here i can use continue instead of res=res+"";
+// }
+// else if(s.charAt(i)=='e'||s.charAt(i)=='E'){
+// res=res+"";
+// }
+// else if(s.charAt(i)=='i'||s.charAt(i)=='I'){
+// res=res+"";
+// }
+// else if(s.charAt(i)=='o'||s.charAt(i)=='O'){
+// res=res+"";
+// }
+// else if(s.charAt(i)=='u'||s.charAt(i)=='U'){
+// res=res+"";
+// }
+// else{
+// res=res+s.charAt(i);
+// }
+// }
+// System.out.println(res);
+// }
+// }
